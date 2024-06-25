c03aac7d0f725ddb2699463955dddc0a0c1984ee
ariling <97501226+Ariling@users.noreply.github.com>
2024-06-25 19:06:06 +0900

    [Silver I] Title: 스티커, Time: 796 ms, Memory: 49300 KB -BaekjoonHub

---
+++ 백준/Silver/9465. 스티커/스티커.py
@@ -0,0 +1,25 @@
+import sys
+input = sys.stdin.readline
+T = int(input())
+
+for _ in range(1, T+1):
+    N = int(input())
+    arr = [list(map(int, input().split())) for _ in range(2)]
+    dp = [[0] * N for _ in range(2)]
+
+    # 스티커 길이가 1일때
+    dp[0][0] = arr[0][0]
+    dp[1][0] = arr[1][0]
+    if N == 1:
+        print(max(dp[0][0], dp[1][0]))
+        continue
+    #스티커 길이가 2일때
+    dp[0][1] = arr[1][0] + arr[0][1]
+    dp[1][1] = arr[0][0] + arr[1][1]
+    if N == 2:
+        print(max(dp[0][1], dp[1][1]))
+        continue
+    for i in range(2, N):
+        dp[0][i] = max(dp[1][i-1], dp[1][i-2]) + arr[0][i]
+        dp[1][i] = max(dp[0][i-1], dp[0][i-2]) + arr[1][i]
+    print(max(dp[0][-1], dp[1][-1]))
