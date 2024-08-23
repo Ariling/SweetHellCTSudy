04a59df35e01a82bed8459a2648d939a3775e52f
ariling <97501226+Ariling@users.noreply.github.com>
2024-08-23 21:19:15 +0900

    [level 2] Title: 호텔 대실, Time: 2.61 ms, Memory: 10.7 MB -BaekjoonHub

--- 프로그래머스/2/155651. 호텔 대실/README.md
+++ 프로그래머스/2/155651. 호텔 대실/README.md
@@ -1,10 +1,10 @@
 # [level 2] 호텔 대실 - 155651 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/155651?language=python3) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/155651) 
 
 ### 성능 요약
 
-메모리: 10.7 MB, 시간: 1.27 ms
+메모리: 10.7 MB, 시간: 2.61 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 05월 13일 23:40:35
+2024년 08월 23일 21:19:13
 
 ### 문제 설명
 
--- 프로그래머스/2/155651. 호텔 대실/호텔 대실.py
+++ 프로그래머스/2/155651. 호텔 대실/호텔 대실.py
@@ -1,11 +1,15 @@
 import heapq
 def solution(book_time):
-    time_arr = [(int(s[:2]) * 60 + int(s[3:]), int(e[:2]) * 60 + int(e[3:])) for s,e in book_time]
-    time_arr.sort()
-    heap = []
-    for i in time_arr:
-        if len(heap) != 0 and i[0] >= heap[0]+10:
-            heapq.heappop(heap)
-        heapq.heappush(heap, i[1])
-    answer = len(heap)
-    return answer
+    # 분 형태로 바꾸고 나서 정렬을 한 뒤, 그거 end 타임 정해두고 하면 되는 거 아닌가?
+    book_time_array = []
+    for start, end in book_time:
+        start_time = (int(start[:2]) * 60) + int(start[3:])
+        end_time = (int(end[:2]) * 60) + int(end[3:]) 
+        book_time_array.append([start_time, end_time])
+    book_time_array = sorted(book_time_array, key=lambda x : (x[0], x[1]))
+    room = []
+    for start, end in book_time_array:
+        if room and start >= room[0]:
+            heapq.heappop(room)
+        heapq.heappush(room, end+10)
+    return len(room)
