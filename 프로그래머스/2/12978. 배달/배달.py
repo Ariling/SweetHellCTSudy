b0aa90108811d54f828e0d6d73485d25f5b8fb65
ariling <97501226+Ariling@users.noreply.github.com>
2024-09-22 08:54:41 +0900

    [level 2] Title: 배달, Time: 1.20 ms, Memory: 10.4 MB -BaekjoonHub

--- 프로그래머스/2/12978. 배달/배달.py
+++ 프로그래머스/2/12978. 배달/배달.py
@@ -1,35 +1,39 @@
 import heapq
-# 이게 큰 수를 만드는 방법
-INF = 1e8
 def solution(N, road, K):
     answer = 0
-    # 1번부터 출발한다고 하기 때문에 이를 적으면 된다.
-    # 음수로 해두면 일단 아닌게 확실하기 때문
-    graph = [[] for _ in range(N+1)]
-    distance = [INF] * (N+1)
-    # 이거 양방향 처리해야되네...
-    for i in range(len(road)):
-        graph[road[i][0]].append((road[i][1], road[i][2]))
-        graph[road[i][1]].append((road[i][0], road[i][2]))
-    dijkstra(1, distance, graph)
-    for num in distance:
-        if num <= K:
+    INF = int(1e9)
+    graph = [[0] * N for _ in range(N)]
+    for s, e, time in road:
+        if graph[s-1][e-1] != 0 and graph[s-1][e-1] <= time:
+            continue
+        # 할당하기 위한 방법, idx는 0부터니깐 그거에 맞춰서!
+        graph[s-1][e-1] = time
+        graph[e-1][s-1] = time
+    # 시작점부터 나타내주는 다익스트라 함수 만들기 (배열이나 q등 다 여기에 넣고 한다.)
+    def dikstra(start):
+        distances = [INF] * N
+        q = []
+        # 시간, 지점 순으로 넣기
+        heapq.heappush(q,(0, start))
+        distances[start] = 0
+        while q:
+            time, now = heapq.heappop(q)
+            # 현재 가지고 있는 거리가 시간 보다 작다면 굳이 할 필요가 없음
+            if distances[now] < time:
+                continue
+            # 도착 지점을 정할 때는 연결지점을 반드시 확인해보기
+            for to in range(N):
+                # 0이면 굳이 밑에 있는 애들을 할 필요가 없다.
+                if graph[to][now] == 0:
+                    continue
+                # 거리 구하는 공식 및 비교 방식
+                dist = time + graph[to][now]
+                if dist < distances[to]:
+                    distances[to] = dist
+                    heapq.heappush(q,(dist, to))
+        return distances
+    dist_arr = dikstra(0)
+    for dist in dist_arr:
+        if dist <= K:
             answer += 1
     return answer
-
-# 이건 좀 참고해서 만들었음...
-def dijkstra(start, distance, graph):
-    q = []
-    heapq.heappush(q, (0, start))
-    distance[start] = 0
-
-    while q:
-        dist, now = heapq.heappop(q)
-
-        if distance[now] < dist:
-            continue
-
-        for i in graph[now]:
-            if dist+i[1] < distance[i[0]]:
-                distance[i[0]] = dist + i[1]
-                heapq.heappush(q, (dist+i[1], i[0]))
