02af5b99befced1dd9ab3b87957496eb01adb16f
ariling <97501226+Ariling@users.noreply.github.com>
2024-10-29 20:48:00 +0900

    [Silver I] Title: 포도주 시식, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Silver/2156. 포도주 시식/포도주 시식.py
@@ -0,0 +1,15 @@
+import sys
+input = sys.stdin.readline
+
+N = int(input())
+arr = [0] * 10000
+dp = [0] * 10000
+for i in range(N):
+    arr[i] = int(input())
+dp[0] = arr[0]
+dp[1] = arr[0] + arr[1]
+dp[2] = max(arr[2]+arr[0], arr[1]+arr[2], dp[1])
+for i in range(3, N):
+    dp[i] = max(arr[i] + dp[i-2], arr[i]+arr[i-1]+dp[i-3], dp[i-1])
+
+print(dp[N-1])
