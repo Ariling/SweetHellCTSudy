1605e26452abc9dd91be1ad510078591c91ffc0a
ariling <97501226+Ariling@users.noreply.github.com>
2024-11-16 23:57:57 +0900

    [level 2] Title: 리코쳇 로봇, Time: 784.32 ms, Memory: 10.4 MB -BaekjoonHub

--- 프로그래머스/2/169199. 리코쳇 로봇/리코쳇 로봇.py
+++ 프로그래머스/2/169199. 리코쳇 로봇/리코쳇 로봇.py
@@ -1,40 +1,56 @@
 from collections import deque
-
 def solution(board):
-    start_x = 0
-    start_y = 0
-    board_arr = [list(items) for items in board]
-    len_r = len(board_arr)
-    len_c = len(board_arr[0])
-    for i in range(len_r):
-        for j in range(len_c):
-            if board_arr[i][j] == 'R':
-                start_x = i
-                start_y = j
-                break
+    INF = int(1e9)
+    answer = INF
+    arr = []
+    for string in board:
+        arr.append([*string])
     dr = [-1, 1, 0, 0]
     dc = [0, 0, -1, 1]
-    visited = set()
-    q = deque()
-    q.append((start_x, start_y, 0))
-    visited.add((start_x, start_y))
-    while q:
-        r, c, move = q.popleft()
-        if board_arr[r][c] == 'G':
-            return move
-        for i in range(4):
-            new_r = r + dr[i]
-            new_c = c + dc[i]
-            while 0 <= new_r < len_r and 0 <= new_c < len_c and board_arr[new_r][new_c] != 'D':
-                new_r += dr[i]
-                new_c += dc[i]
-                # visited.add((new_r, new_c))
-            # 조건 이전 위치로 되돌려놓기
-            new_r -= dr[i]
-            new_c -= dc[i]
-            if (new_r, new_c) not in visited:
-                visited.add((new_r, new_c))
-                q.append((new_r, new_c, move+1))
-                
-    return -1
-    
+    boardR = len(arr)
+    boardC = len(arr[0])
+    isFind = False
+    redX = 0
+    redY = 0
+    for i in range(boardR):
+        for j in range(boardC):
+            if arr[i][j] == 'R':
+                redX = i
+                redY = j
+                break
+    def bfs(x,y):
+        nonlocal answer, isFind
+        q = deque([])
+        q.append((x,y, 0))
+        distance = [[INF] * boardC for _ in range(boardR)]
+        distance[x][y] = 0
+        while q:
+            r, c, dis = q.popleft()
+            if arr[r][c] == 'G':
+                isFind = True
+                answer = min(answer, dis)
+                continue
+            if arr[r][c] != 'G' and dis >= answer:
+                continue
+            else:
+                for i in range(4):
+                    newR = r + dr[i]
+                    newC = c + dc[i]
+                    if newR < 0 or newR >= boardR or newC < 0 or newC >= boardC:
+                        continue
+                    else:
+                        while 0 <= newR < boardR and 0 <= newC < boardC:
+                            if arr[newR][newC] == 'D':
+                                break
+                            else:
+                                newR += dr[i]
+                                newC += dc[i]
+                        newR -= dr[i]
+                        newC -= dc[i]
+                        if dis + 1 < distance[newR][newC]: 
+                            distance[newR][newC] = dis + 1
+                            q.appendleft((newR, newC, dis+1))
+    bfs(redX, redY)
+    if not isFind:
+        answer = -1
+    return answer
