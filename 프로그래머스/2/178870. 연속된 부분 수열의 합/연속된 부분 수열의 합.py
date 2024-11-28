71e6117d756dd8ebef9c074458c8551fecc8dc42
ariling <97501226+Ariling@users.noreply.github.com>
2024-11-28 23:43:54 +0900

    [level 2] Title: 연속된 부분 수열의 합, Time: 311.44 ms, Memory: 51.1 MB -BaekjoonHub

--- 프로그래머스/2/178870. 연속된 부분 수열의 합/연속된 부분 수열의 합.py
+++ 프로그래머스/2/178870. 연속된 부분 수열의 합/연속된 부분 수열의 합.py
@@ -1,22 +1,23 @@
 def solution(sequence, k):
-    # 이거 투 포인터 이용해서 하면 될 것 같은데...
-    # start랑 end포인트 설정해가지고...
-    # 이거 무조건 있는거지..?
-    # 0부터 시작해서 end가 len(sequence)까지 가면 끝나는 걸로... start도 그래야 하나?
-    # min_cnt 로 하고 startIdx, endIdx 설정해서 하는 걸로... 
-    start, end, total = 0, 0, 0
-    min_cnt = 1e9
-    startIdx, endIdx = 1e9, 0
-    while start < len(sequence):
-        if total > k or end == len(sequence):
-            total -= sequence[start]
-            start += 1
-        elif total <= k and end < len(sequence):
-            total += sequence[end]
-            end += 1
-        if total == k and (end - start < min_cnt or (end - start == min_cnt and start < startIdx)):
-            min_cnt = end - start
-            startIdx = start
-            endIdx = end
-    answer = [startIdx, endIdx-1]
+    INF = int(1e9)
+    answer = [INF, INF]
+    st, ed, cur = 0, 0, sequence[0]
+    length = len(sequence)
+    cur_length = INF
+    # ed - st가 제일 짧은 거를 찾아서 하면 되는거잖아
+    while st < length:
+        if cur == k:
+            if ed - st < cur_length:
+                cur_length = ed - st
+                answer = [st, ed]
+            cur -= sequence[st]
+            st += 1
+        elif cur > k and st < length:
+            cur -= sequence[st]
+            st += 1
+        elif ed + 1 < length:
+            ed += 1
+            cur += sequence[ed]
+        else:
+            break
     return answer
