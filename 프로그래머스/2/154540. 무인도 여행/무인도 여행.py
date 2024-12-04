3f876a6558255c6631b1f344cb60c361af2ef78a
ariling <97501226+Ariling@users.noreply.github.com>
2024-12-04 16:04:37 +0900

    [level 2] Title: 무인도 여행, Time: 19.20 ms, Memory: 10.9 MB -BaekjoonHub

--- 프로그래머스/2/154540. 무인도 여행/무인도 여행.py
+++ 프로그래머스/2/154540. 무인도 여행/무인도 여행.py
@@ -1,37 +1,33 @@
 from collections import deque
 def solution(maps):
-    # 이거 그거 활용이다. bfs!
-    maps_arr = [list(map_item) for map_item in maps]
-    check = [[False for _ in range(len(maps_arr[0]))] for _ in range(len(maps_arr))]
     answer = []
-    dr = [-1, 1, 0, 0] # 위 아래 왼 우
-    dc = [0 , 0, -1, 1] 
-    def bfs(r, c):
-        q = deque([])
-        land_scape = 0
-        q.append((r, c))
-        check[r][c] = True
-        land_scape += int(maps_arr[r][c])
+    map_arr = []
+    for map in maps:
+        map_arr.append([*map])
+    checked = [[False] * len(map_arr[0]) for _ in range(len(map_arr))]
+    def seek(i, j):
+        q = deque([(i, j)])
+        checked[i][j] = True
+        dr = [-1, 1, 0, 0]
+        dc = [0, 0, -1, 1]
+        land = int(map_arr[i][j])
         while q:
-            start_r, start_c = q.popleft()
-            for i in range(4):
-                new_r, new_c = start_r + dr[i], start_c + dc[i]
-                if new_r < 0 or new_r >= len(maps_arr) or new_c < 0 or new_c >= len(maps_arr[0]):
-                    continue
-                if check[new_r][new_c] or maps_arr[new_r][new_c] == 'X':
-                    continue
-                else:
-                    check[new_r][new_c] = True
-                    q.append((new_r, new_c))
-                    land_scape += int(maps_arr[new_r][new_c])
-        return land_scape
-    for i in range(len(maps_arr)):
-        for j in range(len(maps_arr[i])):
-            if not maps_arr[i][j] == 'X' and not check[i][j]:
-                land = bfs(i, j)
-                answer.append(land)
-    if len(answer) == 0:
+            r, c = q.popleft()
+            for k in range(4):
+                newR = r + dr[k]
+                newC = c + dc[k]
+                if 0 <= newR < len(map_arr) and 0 <= newC < len(map_arr[0]):
+                    if map_arr[newR][newC] != 'X' and not checked[newR][newC]:
+                        checked[newR][newC] = True
+                        q.append((newR, newC))
+                        land += int(map_arr[newR][newC])
+        answer.append(land)
+        return
+    for i in range(len(map_arr)):
+        for j in range(len(map_arr[i])):
+            if map_arr[i][j] != 'X' and not checked[i][j]:
+                seek(i, j)
+    if not answer:
         return [-1]
     else:
-        answer = sorted(answer)
-        return answer
+        return sorted(answer)
